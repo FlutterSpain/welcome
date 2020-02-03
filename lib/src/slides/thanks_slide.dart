4a2512a715d8c1165218411483a7730fa4dd421f
Alberto Chamorro <a.chamorro.ruiz@accenture.com>
2020-02-03 11:49:38 +0100

    :dizzy: Added first version of blue_heart animation

--- lib/src/slides/thanks_slide.dart
+++ lib/src/slides/thanks_slide.dart
@@ -1,17 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
-class ThanksSlide extends StatelessWidget {
+class ThanksSlide extends StatefulWidget {
   const ThanksSlide({Key key}) : super(key: key);
 
   @override
+  _ThanksSlideState createState() => _ThanksSlideState();
+}
+
+class _ThanksSlideState extends State<ThanksSlide> {
+
+  bool _textVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    Future.delayed(Duration(seconds: 4), () {
+      setState(() {
+        _textVisible = true;
+      });
+    });
+    
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+      backgroundColor: Color(0xe613b9fd),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('¡Gracias!', style: Theme.of(context).textTheme.display3),
-            Text('💙', style: Theme.of(context).textTheme.display3),
+            Container(
+              height: 300,
+              width: 300,
+              child: FlareActor(
+                'assets/images/blue_heart.flr',
+                animation: 'thanks',
+              ),
+            ),
+            AnimatedOpacity(
+              opacity: _textVisible ? 1.0 : 0.0,
+              duration: Duration(seconds: 1),
+              curve: Curves.easeInOut,
+              child: Text('¡Gracias!', style: Theme.of(context).textTheme.body2.copyWith(
+                fontSize: 40.0
+              ))
+            )
           ]
         ),
       )

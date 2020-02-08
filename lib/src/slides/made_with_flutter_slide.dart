063ed005b764f768b6cb26250db3ce7992c14f42
Jaime Blasco <git@jaimeblasco.com>
2020-02-08 14:07:14 +0100

    :dizzy: Add animation of background during MadeWithFlutterSlide transition

--- lib/src/slides/made_with_flutter_slide.dart
+++ lib/src/slides/made_with_flutter_slide.dart
@@ -6,19 +6,29 @@ class MadeWithFlutterSlide extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Hecho con Flutter', style: Theme.of(context).textTheme.display3.copyWith(
-              color: Colors.white
-            )),
-            Text('https://pub.dev/packages/flutter_keynote', style: Theme.of(context).textTheme.display1.copyWith(
-              color: Color(0xe613b9fd)
-            )),
-          ]
-        ),
+      backgroundColor: Colors.white,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Hero(
+            tag: 'right_background',
+              child: Container(
+            color: Theme.of(context).backgroundColor,
+          )),
+          Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Hecho con Flutter', style: Theme.of(context).textTheme.display3.copyWith(
+                      color: Colors.white
+                  )),
+                  Text('https://pub.dev/packages/flutter_keynote', style: Theme.of(context).textTheme.display1.copyWith(
+                      color: Color(0xe613b9fd)
+                  )),
+                ]
+            ),
+          )
+        ],
       )
     );
   }
